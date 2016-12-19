87b56ac3084d4811d66b0e8e838c80e498236bbb
tamarit <tamarit27@gmail.com>
2016-12-19 01:06:51 +0100

    First  real code added
    - Temporally removed external repository

--- Makefile
+++ Makefile
@@ -6,10 +6,14 @@ compile:
 	@rm -Rf ebin
 	@mkdir ebin
 	@erlc -o ebin src/*.erl 
+	@cd csp_tracker; make
 
 clean:
 	@rm -Rf ebin
 	@rm -f *.txt
+
+run:
+	erl -pa ebin csp_tracker/ebin -run csp_reversible run csp_tracker/examples/ex3.csp -noshell -s erlang halt
 
 # install:
 # 	@erl -pa ebin -run make_script from_path $(ROOT_DIR)  -noshell -s erlang halt
--- src/csp_reversible.erl
+++ src/csp_reversible.erl
@@ -8,7 +8,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 run(File) -> 
-	run(File,'MAIN').
+	run(File,"MAIN").
 
 run(File, FirstProcess) -> 
-	ok.
+	io:format("~s\n", [File]),
+	OutputFile = 
+		File ++ "_temp",
+	csp_tracker:rewrite_renamings(File, OutputFile),
+	AbsOutputFile = 
+		filename:absname(OutputFile),
+	% io:format("s\n", [AbsOutputFile]),
+	OutputConversion = 
+		os:cmd("cd csp_tracker;./createoutput.sh " ++ AbsOutputFile),
+	io:format("~s\n",[OutputConversion]),
+	csp_tracker:preprocess_variables("csp_tracker/"),
+	case file:consult("csp_tracker/output_rewritten.txt") of
+		{error,{_,_,InfoError}} ->
+			io:format(
+				"Error reading Erlang translation:\n~s\n",
+				[lists:flatten(erl_parse:format_error(InfoError))]),
+			io:format("Correct the syntax error before to proceed\n"),
+			ok;
+		{ok, ProcessList} ->
+			io:format("~p\n", [ProcessList])
+	end.
